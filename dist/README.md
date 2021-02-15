8a0f655f1fe8a1d6a04f1b9aea446adfc33c168a
Benjamin Stanfield <bstanfield1992@gmail.com>
2021-02-14 23:37:38 -0700

    generated README Successfully

---
+++ dist/README.md
@@ -0,0 +1,33 @@
+
+<h1 align="center">ReadMe Generator 👋</h1>
+  
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)<br />
+## Description
+undefined
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+Inquirer
+## Usage
+create README files
+## License
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+<br />
+This application is covered by the MIT license. 
+## Contributing
+undefined
+## Tests
+
+## Questions
+Email<br />
+<br />
+Find me on GitHub: [undefined](https://github.com/undefined)<br />
+<br />
+Email me with any questions: bstanfield1992@gmail.com<br /><br />
+    
--- index.js
+++ index.js
@@ -1,72 +1,95 @@
+// TODO: Include packages needed for this application
+
 const fs = require("fs");
-const inquire = require("inquire");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./util/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser() {
-    return inquire.prompt ([
-        {
-            type: "input" ,
-            name: "projectTitle" ,
-            message: "What is the title of your Project?"
-        },
-        {
-            type: "input" ,
-            name: "Description" ,
-            message: "Please write a brief description of your project: "
-        },
-        {
-            type: "input" ,
-            name: "installation" ,
-            message: "Please describe the installation process if any: "
-        },
-        {
-            type: "input" ,
-            name: "usage" ,
-            message: "Please describe what this project would be used for: "
-        },
-        {
-            type: "list" ,
-            name: "license" ,
-            message: "Please choose the appropriate license for this project: " ,
-            choices:[
-                "Apache",
-                "Academic",
-                "GNU",
-                "ISC",
-                "MIT",
-                "Mozilla",
-                "Open"
-            ]
-        },
-        {
-            type: "input" ,
-            name: "contributions" ,
-            message: "Please list those who contributed to this project: "
-        },
-        {
-            type: "input" ,
-            name: "tests" ,
-            message: "Is there a test included?"
-        },
-        {
-            type: "input" ,
-            name: "questions" ,
-            message: "What should I do if I have any questions?"
-        },
-        {
-            type: "input" ,
-            name: "user" ,
-            message: "What is your GitHub username?"
-        },
-        {
-            type: "input" ,
-            name: "email" ,
-            message: "What is your email?"
-        }
-    ])
-
-    
+// TODO: Create an array of questions for user input
+function promptUser(){
+ return inquirer.prompt ([
+    {
+        type: "input" ,
+        name: "projectTitle" ,
+        message: "What is the title of your Project?"
+    },
+    {
+        type: "input" ,
+        name: "Description" ,
+        message: "Please write a brief description of your project: "
+    },
+    {
+        type: "input" ,
+        name: "installation" ,
+        message: "Please describe the installation process if any: "
+    },
+    {
+        type: "input" ,
+        name: "usage" ,
+        message: "Please describe what this project would be used for: "
+    },
+    {
+        type: "list" ,
+        name: "license" ,
+        message: "Please choose the appropriate license for this project: " ,
+        choices:[
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input" ,
+        name: "contributions" ,
+        message: "Please list those who contributed to this project: "
+    },
+    {
+        type: "input" ,
+        name: "tests" ,
+        message: "Is there a test included?"
+    },
+    {
+        type: "input" ,
+        name: "questions" ,
+        message: "What should I do if I have any questions?"
+    },
+    {
+        type: "input" ,
+        name: "user" ,
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input" ,
+        name: "email" ,
+        message: "What is your email?"
+    }
+]);
 }
 
-function init() {
-    
+
+// TODO: Create a function to write README file
+// function writeToFile(filename, data) {
+//     generateReadme(data);
+// }
+
+// TODO: Create a function to initialize app
+async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
 }
+
+// Function call to initialize app
+init();
